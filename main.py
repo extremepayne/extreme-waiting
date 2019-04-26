476a4b0455c86057afbb5534cb4b60e2e89f8917
extremepayne <harrison.payne@gmail.com>
2019-04-25 21:32:04 -0600

    not working

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from datetime import date
+import math
 
-cur_date = date.today()
-print(cur_date.year)
+CUR_DATE = date.today()
+print(CUR_DATE.year)
+
+
+def find_date(todays_date, progress):
+    """Find a date according to XKCD 1017"""
+    return math.exp((20.3444 * (progress ** 3)) + 3) - math.exp(3)
+
+
+i = 1
+while i <= 100:
+    print(find_date(CUR_DATE.year, i))
+    i += 1
+input("press enter to exit")
