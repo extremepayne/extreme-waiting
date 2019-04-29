539d34b0a41984ac7f1834caef8730c3dae70b1b
extremepayne <46696318+extremepayne@users.noreply.github.com>
2019-04-29 08:18:46 -0600

    more facts

--- facts.py
+++ facts.py
@@ -9,16 +9,22 @@ go extinct.",
 plants.",
     225000000: "225 million years ago: First dinosaurs.",
     430000000: "430 million years ago: First plants.",
+    530000000: "530 million years ago: First chordates.",
     600000000: "600 million years ago: First \
 multicellular organisms.",
     1850000000: "1.9 billion years ago: First \
 eukaryotes.",
     3700000000: "3.7 billion years ago: Life begins.",
+    4600000000: "4.6 billion years ago: Solar system forms.",
+    13300000000: "13.4 billion years ago: Galaxies form.",
     13400000000: "13.7 billion years ago: Universe forms.",
 }
 
 facts_bc = {
+    3300: "3300 BC: Indus Valley civilization begins in India.",
     1600: "1600 BC: Shang dynasty begins in China.",
     1046: "1046 BC: Zhou dynasty begins in China.",
+    900: "900 BC: Chavin civilization rises in South America.",
+    322: "322 BC: Maurya Empire begins in India.",
     221: "221 BC: Han dynasty begins in China.",
 }
--- main.py
+++ main.py
@@ -39,7 +39,7 @@ while i <= 1:
         time.sleep(0.03)
         i += 0.01
     else:
-        if result > 300000:
+        if result > 4000:
             for key, value in facts.facts_ma.items():
                 if key > result:
                     to_p = (
@@ -51,6 +51,8 @@ while i <= 1:
                     sys.stdout.write(to_p)
                     sys.stdout.flush()
                     break
+            time.sleep(0.03)
+            i += 0.0003
         else:
             year_bc = abs(CUR_DATE.year - result)
             for key, value in facts.facts_bc.items():
@@ -64,6 +66,6 @@ while i <= 1:
                     sys.stdout.write(to_p)
                     sys.stdout.flush()
                     break
-        time.sleep(0.03)
-        i += 0.001
+            time.sleep(0.03)
+            i += 0.0001
 input("press enter to exit                       ")
