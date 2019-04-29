e88f4534b090efb26b49295d1b98f141ceba1f73
extremepayne <46696318+extremepayne@users.noreply.github.com>
2019-04-29 14:02:34 -0600

    Allow selection of wait time.

--- main_with_curses.py
+++ main_with_curses.py
@@ -24,7 +24,49 @@ def find_date(todays_date, progress):
         return my_date.strftime("%B %d %Y"), my_date, True
     return str(round(return_date)) + " years ago", return_date, False
 
+def ask(prompt, type_=None, min_=None, max_=None, range_=None):
+    """Get user input of a certain type, with range and min/max options."""
+    if min_ is not None and max_ is not None and max_ < min_:
+        raise ValueError("min_ must be less than or equal to max_.")
+    while True:
+        ui = input(prompt)
+        if type_ is not None:
+            try:
+                ui = type_(ui)
+            except ValueError:
+                print("Input type must be {0}.".format(type_.__name__))
+                continue
+        if max_ is not None and ui > max_:
+            print("Input must be less than or equal to {0}.".format(max_))
+        elif min_ is not None and ui < min_:
+            print("Input must be greater than or equal to {0}.".format(min_))
+        elif range_ is not None and ui not in range_:
+            if isinstance(range_, range):
+                template = "Input must be between {0.start} and {0.stop}."
+                print(template.format(range_))
+            else:
+                template = "Input must be {0}."
+                if len(range_) == 1:
+                    print(template.format(*range_))
+                else:
+                    print(
+                        template.format(
+                            " or ".join(
+                                (
+                                    ", ".join(map(str, range_[:-1])),
+                                    str(range_[-1]),
+                                )
+                            )
+                        )
+                    )
+        else:
+            return ui
 
+
+
+mins = ask("How long are you waiting for? (minutes) ", float, 0)
+secs = mins*60
+wait_time = secs/1000
 CUR_DATE = date.today()
 try:
     stdscr = curses.initscr()
@@ -61,11 +103,11 @@ try:
                         break
                 if not done:
                     to_p = "No facts for this era."
-        stdscr.addstr(6, 5, (str(to_p) + " "*10), curses.A_BOLD)
+        stdscr.addstr(6, 5, (str(to_p) + " "*13), curses.A_BOLD)
         curses.doupdate()
         stdscr.refresh()
         i+=0.001
-        time.sleep(0.03)
+        time.sleep(wait_time)
     stdscr.addstr(7, 5, 'Press q to close this screen', curses.A_NORMAL)
     while True:
         # stay in this loop till the user presses 'q'
