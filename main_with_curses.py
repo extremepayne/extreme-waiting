75f59235a3f6a013cada90de1ad248be066c8e91
extremepayne <46696318+extremepayne@users.noreply.github.com>
2019-04-29 13:51:05 -0600

    Working :tada:

--- main_with_curses.py
+++ main_with_curses.py
@@ -1,18 +1,8 @@
-try:
-    import curses
-except ImportError:
-    print("This dosen't work on your platform. Run main.py instead.")
-    input("Press enter to exit.")
-    quit()
+import curses
 from datetime import date, datetime, timedelta
 import math
 import time
-import sys
 import facts
-
-CUR_DATE = date.today()
-# print(CUR_DATE.year)
-
 
 def decimal_year_to_date(decimal_year):
     """Change YYYY.YYYY to MM DD YYYY."""
@@ -34,51 +24,54 @@ def find_date(todays_date, progress):
         return my_date.strftime("%B %d %Y"), my_date, True
     return str(round(return_date)) + " years ago", return_date, False
 
-stdscr = curses.initscr()
-curses.noecho()
-curses.curs_set(False)
 
-i = 0
-while i <= 1:
-    to_p, result, res_type = find_date(CUR_DATE, i)
-    if res_type:
-        to_p = str(int(i * 100)) + "%       " + to_p + "     \r"
-        sys.stdout.write(to_p)
-        sys.stdout.flush()
-        time.sleep(0.03)
-        i += 0.01
-    else:
-        if result > 8000:
-            for key, value in facts.facts_ma.items():
-                if key > result:
-                    to_p = (
-                        str(int(i * 100))
-                        + "%"
-                        + str(value)
-                        + "\r"
-                    )
-                    sys.stdout.write(to_p)
-                    sys.stdout.flush()
-                    break
-            time.sleep(0.03)
-            i += 0.0003
+CUR_DATE = date.today()
+try:
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(0)
+    stdscr.keypad(1)
+    stdscr.border(0)
+    i = 0
+    #while i <= 1:
+        #to_p, result, res_type = find_date(CUR_DATE, i)
+        #if res_type:
+    #stdscr.addstr(5, 5, "str(i)", curses.A_BOLD)
+    #i+=0.01
+    #time.sleep(1)
+    while i <= 1:
+        to_p, result, res_type = find_date(CUR_DATE, i)
+        stdscr.addstr(5, 5, (str(int(i*100)) + "%"), curses.A_BOLD)
+        if res_type:
+            pass
         else:
-            year_bc = abs(CUR_DATE.year - result)
-            for key, value in facts.facts_bc.items():
-                if key < year_bc:
-                    to_p = (
-                        str(int(i * 100))
-                        + "%"
-                        + str(value)
-                        + "\r"
-                    )
-                    sys.stdout.write(to_p)
-                    sys.stdout.flush()
-                    break
-            time.sleep(0.03)
-            i += 0.0001
-input("press enter to exit")
-
-curses.echo()
-curses.curs_set(True)
-curses.endwin()
+            if result > 8000:
+                for key, value in facts.facts_ma.items():
+                    if key > result:
+                        to_p = str(value)
+                        break
+            else:
+                year_bc = abs(CUR_DATE.year - result)
+                for key, value in facts.facts_bc.items():
+                    if key < year_bc:
+                        to_p = str(value)
+                        break
+        stdscr.addstr(6, 5, (str(to_p) + " "*10), curses.A_BOLD)
+        curses.doupdate()
+        stdscr.refresh()
+        i+=0.01
+        time.sleep(0.03)
+    stdscr.addstr(7, 5, 'Press q to close this screen', curses.A_NORMAL)
+    while True:
+        # stay in this loop till the user presses 'q'
+        ch = stdscr.getch()
+        if ch == ord('q'):
+            break
+    
+finally:
+    stdscr.keypad(0)
+    curses.echo()
+    curses.nocbreak()
+    curses.curs_set(1)
+    curses.endwin()
